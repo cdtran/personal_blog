6ed77e104fd7a7a3dcec6c213047d0316a601fd2
Chris <ktranman@gmail.com>
2017-07-13 00:09:24 -0700

    navbar and basic pages

--- app/views.py
+++ app/views.py
@@ -3,5 +3,16 @@ from flask import render_template
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('base.html')
+    return render_template('base.html', page='home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', page='about')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', page='contact')
